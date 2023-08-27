1cd5a832c86ff461681f4775fd603f98d88accc9
Rachel Kim <hyunjinney@naver.com>
2023-08-27 17:37:55 +0900

    [Fix] Hp
    충돌시 체력 증가 및 오브젝트 비활성화

--- Assets/Scripts/ScriptableObjects/HealthIncreaseTrigger.cs
+++ Assets/Scripts/ScriptableObjects/HealthIncreaseTrigger.cs
@@ -7,12 +7,15 @@ public class HealthIncreaseTrigger : MonoBehaviour
     [SerializeField, Tooltip("How much should the player's health Increase")]
     private int helathIncreaseAmount = 10;
 
+    [SerializeField]
+    private HealthScriptableObject healthManager;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-
+            healthManager.IncreaseHealth(helathIncreaseAmount);
+            gameObject.SetActive(false);
         }
     }
 }
